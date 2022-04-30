3dcbb2e78368b1ce2b27457c6ff8000e6cdbbdfc
anajuliarauber <anajuliarauber@hotmail.com>
2022-04-30 11:36:39 -0500

    Adicionando componente ChatModal e adicionando email e telefone no UserForm

---
+++ web/src/components/ChatModal/index.js
@@ -0,0 +1,36 @@
+import Modal from 'react-modal'
+import { useState } from 'react'
+import './styles.css'
+import chatImg from '../../assets/chat-img.svg'
+
+Modal.setAppElement('#root');
+
+export function ChatModal({user}) {
+    const [showModal, setShowModal] = useState(false);
+  
+    function handleOpenModal() {
+      setShowModal(true);
+    }
+  
+    function handleCloseModal() {
+      setShowModal(false);
+    }
+  
+    return (
+      <div>
+        <img onClick={handleOpenModal} src={chatImg} width='25px'></img>
+        <Modal
+          isOpen={showModal}
+          contentLabel="onRequestClose Example"
+          onRequestClose={handleCloseModal}
+          shouldCloseOnOverlayClick={true}
+          overlayClassName= "modal-overlay"
+          className="modal-content"
+        >
+          <h2>Ficamos felizes por ter encontrado um match!</h2>
+          <p>Entre em contato para pedalar junto com {user.name} pelo telefone {user.telefone} ou pelo email {user.email}</p>
+          <button onClick={handleCloseModal}>Fechar</button>
+        </Modal>
+      </div>
+    );
+  }
